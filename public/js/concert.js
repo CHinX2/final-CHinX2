7cc3b845a06e1d6574ac2fbd26eeb2acfcd91a06
CHinX2 <iris870425@gmail.com>
2019-06-24 21:49:32 +0800

    Update concert.js

--- public/js/concert.js
+++ public/js/concert.js
@@ -54,7 +54,7 @@ class Concert {
 
     async _loadComm() {
       console.log(this.id);
-      const result = await fetch('/get/'+this.id);
+      const result = await fetch('/get/'+$this.id);
       const json = await result.json();
       
       if(json) {
@@ -63,7 +63,7 @@ class Concert {
         this.comm = json.comm;
       }
 
-      document.addEventListener('submit',this._saveComm);
+      document.addEventListener('click',this._saveComm);
     }
 
     async _saveComm() {
